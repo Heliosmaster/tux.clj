fb11881de5cad66f2f1e5207e3ac5692eec20655
Davide Taviani <helios89@gmail.com>
2015-09-24 16:26:45 +0200

    Fix and better tests

--- test/tux/test/handler.clj
+++ test/tux/test/handler.clj
@@ -3,12 +3,20 @@
             [tux.handler :refer :all]
             [ring.mock.request :as mock]))
 
+(deftest test-helpers
+  (testing "valid-url?"
+    (is (valid-url? "https://tux.it"))
+    (is (valid-url? "http://tux.it"))
+    (is (not (valid-url? "tux.it")))))
+
+(deftest test-database
+  (testing "setting and getting a value"
+    (let [long-url "https://tux.it"
+          short-url "abc"]
+      (set-mapping short-url long-url)
+      (is (= long-url (get-mapping short-url))))))
+
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 200)))))
