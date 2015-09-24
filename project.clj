42f802998d7048cf50793c1c6b7fda8561dc4611
Davide Taviani <helios89@gmail.com>
2015-09-24 16:04:26 +0200

    Update Clojure and dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject tux "0.1.0-SNAPSHOT"
   :description "Simple URL shortener in Clojure"
   :url "http://tux.it"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [hiccup "1.0.5"]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
                  [org.clojure/tools.logging "0.3.0"]
-                 [com.taoensso/carmine "2.7.0-RC1"]
-                 [compojure "1.1.8"]]
-  :plugins [[lein-ring "0.8.11"]]
+                 [com.taoensso/carmine "2.11.1"]
+                 [compojure "1.4.0"]]
+  :plugins [[lein-ring "0.9.6"]]
   :ring {:handler tux.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring/ring-mock "0.3.0"]]}})
